98aa54b73503504f8fb38584b4695412318ed1bc
Giorgio Braz <29463682+giorgiobraz@users.noreply.github.com>
2022-01-08 14:38:47 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Giorgio Braz on 08/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
